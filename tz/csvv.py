284a48444a6c64af313ceed869790393403fd9a2
Roman <42977616+ivorobey@users.noreply.github.com>
2021-02-02 17:53:22 +0200

    notyet

--- tz/csvv.py
+++ tz/csvv.py
@@ -1,4 +1,4 @@
-# import csv
+
 
 # with open('names.csv', 'w') as csvfile:
 #     fieldnames = ['first_name', 'last_name']
@@ -9,14 +9,35 @@
 #     writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
 #     writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
 
+import csv
 import string
 import random
+
+
 
 def generator(size=1024,chars=string.ascii_lowercase+string.digits):
     return ["".join(random.choice(chars)for _ in range(size))]
     
 
 mylist=generator()
+
+print(mylist)
+
+
+lol = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
+for el in mylist:
+    a=lol(el,8)
+
+print(a)
+
+
+
+
+
+# with open('list_to_csv.csv','w') as csvfile:
+#     csvwriter=csv.writer(csvfile)
+#     for item in a:
+#         csvwriter.writerow([item])
 
 
 
