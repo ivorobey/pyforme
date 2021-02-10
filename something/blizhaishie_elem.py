34ef00b340deea85f8ca7ca3d9ebfabc763c8ca7
Roman <42977616+ivorobey@users.noreply.github.com>
2021-02-10 11:36:01 +0200

    okk

--- something/blizhaishie_elem.py
+++ something/blizhaishie_elem.py
@@ -4,6 +4,7 @@ l = [1, 4, 8, 3, 10, 5, 11]
 # lambda - это анонимная функция которая часто используется в таких функциях, как sorted()\ min
 #       которые принимают вызываемый объект в качестве параметра (часто параметр ключевого слова key )
 
+
 def search_min_max(lst, target):
     lower = min([i for i in lst if i < target], key=lambda x: abs(x-target))
     upper = min([i for i in lst if i >= target], key=lambda x: abs(x-target))
